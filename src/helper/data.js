f9f92f56f07031ec459bd4b7568483f0242aea94
Lex <Lexor.V.A.S@yandex.ru>
2022-06-23 06:18:46 +0300

    main part_1.1

---
+++ src/helper/data.js
@@ -0,0 +1,118 @@
+import { nanoid } from 'nanoid'
+export const testData = [
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'design',
+    url:'https://www.shutterstock.com/ru/image-vector/vector-graphic-template-blue-hues-monochromatic-2106655082'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'branding',
+    url:'https://www.shutterstock.com/ru/image-vector/application-smartphone-business-graph-analytics-data-1583248045'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'design',
+    url:'https://www.shutterstock.com/ru/image-vector/concept-application-testing-debugging-development-process-1147591109'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'motion',
+    url:'https://www.shutterstock.com/ru/image-vector/development-nuclear-atomic-technology-interaction-different-1329452954'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'branding',
+    url:'https://www.shutterstock.com/ru/image-vector/isometric-team-discussion-748983985'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'design',
+    url:'https://www.shutterstock.com/ru/image-photo/researcher-blood-sample-new-variant-covid19-2081553577'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'motion',
+    url:'https://www.shutterstock.com/ru/image-photo/medicine-doctor-hand-working-modern-digital-414505303'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'branding',
+    url:'https://www.shutterstock.com/ru/image-photo/doctor-nurse-followup-data-medical-examination-2030192909'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'design',
+    url:'https://www.shutterstock.com/ru/image-vector/code-testing-cartoon-banner-functional-test-1824823199'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'motion',
+    url:'https://www.shutterstock.com/ru/image-vector/big-data-analytics-computing-center-monitor-2044867709'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'branding',
+    url:'https://www.shutterstock.com/ru/image-photo/doctor-scientist-analysis-diagnose-checking-blood-1687180639'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'design',
+    url:'https://www.shutterstock.com/ru/image-illustration/application-security-testing-concept-digital-magnifying-2128034039'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'motion',
+    url:'https://www.shutterstock.com/ru/image-photo/big-data-database-app-bits-access-547118830'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'illustration',
+    url:'https://www.shutterstock.com/ru/image-illustration/abstract-style-illustration-depicting-printed-circuit-161498747'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'design',
+    url:'https://www.shutterstock.com/ru/image-vector/website-loading-optimization-page-speed-seo-2044562390'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'motion',
+    url:'https://www.shutterstock.com/ru/image-photo/two-colleague-web-designer-discussing-data-507042697'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'illustration',
+    url:'https://www.shutterstock.com/ru/image-photo/agriculturist-utilize-core-data-network-internet-1884814591'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'motion',
+    url:'https://www.shutterstock.com/ru/image-vector/technical-support-system-software-development-landing-2078179042'
+},
+{
+    id:nanoid(8),
+    name:`test ${nanoid(2)}`,
+    category:'branding',
+    url:'https://www.shutterstock.com/ru/image-photo/young-guy-student-prepare-research-essay-1906252684'
+},
+
+]
